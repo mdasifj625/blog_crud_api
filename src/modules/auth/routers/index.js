b0c71553cd14a408b284f91183068f6c21cf682d
MD ASIF JAWED <mdasifj625@gmail.com>
2023-06-18 20:41:49 +0530

    user signup complete

--- src/modules/auth/routers/index.js
+++ src/modules/auth/routers/index.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
-// import AuthController from '../controller'
+import AuthController from '../controller/index.js'
 
 // import { authorize } from '@middlewares/authorizer'
 
-// const authController = new AuthController()
+const authController = new AuthController()
 const router = Router()
 
 router.get('/ping', (_req, res) => {
   return res.success()
 })
 
-// router.post('/signup', Wrap(authController.Signup))
+router.post('/signup', authController.Signup.bind())
 
 // router.post('/signin', Wrap(authController.Signin))
 
